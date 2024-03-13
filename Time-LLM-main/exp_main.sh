14de2df73d61c15463fdc8f75d1f71d913df6275
Ralphson <673091663@qq.com>
2024-03-13 15:06:20 +0800

    0

--- Time-LLM-main/exp_main.sh
+++ Time-LLM-main/exp_main.sh
@@ -12,7 +12,7 @@ comment='TimeLLM-ETTh1'
 # 正式训练
 master_port=00097
 num_process=1
-batch_size=36
+batch_size=96
 train_epochs=10
 accelerate launch --mixed_precision bf16 --num_processes $num_process --main_process_port $master_port run_main.py \
   --task_name long_term_forecast \
