20240315: fix masked dataset bug, fix attention mask, fix 5 metrics in logger, update smape->mse. But file names are inversed :(

Time-LLM-main/data_provider/data_factory.py
@@ -1,4 +1,4 @@
-from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4, Dataset_Masked_Battery
+from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4, Dataset_Masked_Battery, Dataset_Battery
 from torch.utils.data import DataLoader
 
 data_dict = {
@@ -9,6 +9,7 @@ data_dict = {
     'custom': Dataset_Custom,
     'm4': Dataset_M4,
     'masked_battery': Dataset_Masked_Battery,
+    'masked_battery_temp': Dataset_Battery
 }
 
 
@@ -42,6 +43,22 @@ def data_provider(args, flag):
             seasonal_patterns=args.seasonal_patterns
         )
     elif args.data == 'masked_battery':
+        data_set = Data(
+            root_path=args.root_path,
+            data_path=args.data_path,
+            flag=flag,
+            size=[args.seq_len, args.label_len, args.pred_len],
+            features=args.features,
+            target=args.target,
+            timeenc=timeenc,
+            freq=freq,
+            percent=percent,
+            seasonal_patterns=args.seasonal_patterns,
+            cutting_rate=args.cutting_rate,
+            drop_bid=args.drop_bid,
+            seq_limit=args.seq_limit
+        )
+    elif args.data == 'masked_battery_temp':
         data_set = Data(
             root_path=args.root_path,
             data_path=args.data_path,

Time-LLM-main/data_provider/data_loader.py
@@ -534,11 +534,11 @@ class Dataset_Battery(Dataset):
     def __init__(self, root_path, flag='train', size=None,
                  features='S', data_path='trimmed_LX3_ss0_se100_cr05_C_V_T_vs_CE.csv',
                  target='OT', scale=True, timeenc=0, freq='h', percent=100,
-                 seasonal_patterns=None, cutting_rate=1.2, drop_bid=False):
+                 seasonal_patterns=None, cutting_rate=1.2, drop_bid=False, seq_limit=0):
         if size == None:
-            self.seq_len = 96    # 训练长度
-            self.label_len = 48  # 重叠部分,用于预测序列回看知识
-            self.pred_len = 96   # 预测
+            self.seq_len = 24    # 训练长度
+            self.label_len = 12  # 重叠部分,用于预测序列回看知识
+            self.pred_len = 36   # 预测
         else:
             self.seq_len = size[0]
             self.label_len = size[1]
@@ -550,6 +550,7 @@ class Dataset_Battery(Dataset):
 
         self.drop_bid = drop_bid    # TODO:battery_id可能需要独热编码,考虑drop或者不进行归一化
         self.cutting_rate = cutting_rate
+        self.seq_limit = seq_limit
         self.percent = percent
         self.features = features
         self.target = target
@@ -569,6 +570,9 @@ class Dataset_Battery(Dataset):
 
         # 按照电池+轮次打乱
         indexed_df = df_raw.set_index(['battery_id', 'Cycle_Index'])
+        indexed_df['luncishu'] = df_raw.groupby(['battery_id', 'Cycle_Index']).count()['cycle_test_time']
+        indexed_df = indexed_df[indexed_df['luncishu'] > self.seq_limit]   # 只保留<48>以上的轮次
+        indexed_df = indexed_df.drop('luncishu', axis=1)        # 删除luncishu
         shuffled_index = indexed_df.index.unique().values
         random.shuffle(shuffled_index)
         shuffled_index = pd.Index(shuffled_index)
@@ -619,9 +623,6 @@ class Dataset_Battery(Dataset):
 
         # 等长数据
         self.data = np.array([self.data[index] for index in self.recover_index_list], dtype=object)
-
-        # TODO
-
         print('data-{} load completed: {}'.format(self.set_type, self.data.shape))
 
     def __getitem__(self, index):
@@ -631,9 +632,14 @@ class Dataset_Battery(Dataset):
         outsample_mask = np.zeros((self.label_len + self.pred_len, 1))
 
         seq = self.data[index]
-        cut_point = np.random.randint(low=max(1, len(seq) - int(self.pred_len * self.cutting_rate)),
-                                      high=len(seq),
-                                      size=1)[0]    # 切分点,
+        try:
+            low = max(1, len(seq) - int(self.pred_len * self.cutting_rate))
+            high = len(seq)
+            cut_point = np.random.randint(low=low,
+                                          high=high,
+                                          size=1)[0]    # 切分点,
+        except:
+            raise ValueError("index={}, low={}, high={}".format(index, low, high), len(seq))
 
         # 输入padding mask
         insample_window = seq[max(0, cut_point - self.seq_len): cut_point]

Time-LLM-main/models/BatteryGPT_mask.py
@@ -151,9 +151,9 @@ class Model(nn.Module):
         n_vars = x_enc.shape[-1]
         source_embeddings = self.mapping_layer(self.word_embeddings.permute(1, 0)).permute(1, 0)
         x_enc = x_enc.permute(0, 2, 1).contiguous()                         # [batch_size, nvars, seq_len]
-        # x_mark_enc = x_mark_enc.permute(0, 2, 1).contiguous()               # [batch_size, nvars, seq_len]
-        enc_out, n_vars, x_mask = self.patch_embedding(x_enc.to(torch.bfloat16))
-        # enc_out, x_mask = self.seq_embedding(x_enc.to(torch.bfloat16), x_mark_enc)
+        x_mark_enc = x_mark_enc.permute(0, 2, 1).contiguous()               # [batch_size, nvars, seq_len]
+        enc_out, n_vars, x_mask = self.patch_embedding(x_enc.to(torch.bfloat16), x_mark_enc)
+        # enc_out, x_mask = self.seq_embedding(x_enc.to(torch.bfloat16), x_mark_enc)    # no_patch_embedding
         enc_out = self.reprogramming_layer(enc_out, source_embeddings, source_embeddings)
         
 
@@ -228,11 +228,13 @@ class ReprogrammingLayer(nn.Module):
         scores = scale * scores
 
         # 掩码
-        # if x_mask is not None:
-        #     x_mask = x_mask.unsqueeze(1)[:,:,:,0].unsqueeze(-1)
-        #     x_mask = ~x_mask.to(torch.bool)
-        #      scores = scores.masked_fill(x_mask, float('-inf'))
-
+        if x_mask is not None:
+            x_mask = x_mask.unsqueeze(1)[:,:,:,0].unsqueeze(-1)
+            x_mask = ~x_mask.to(torch.bool)
+            scores = scores.masked_fill(x_mask, float('-inf'))
+        # print(torch.isnan(scores).any())    # false
+        # print(torch.isinf(scores).any())    # false
+        
         scores = torch.softmax(scores, dim=-1)
 
         A = self.dropout(scores)