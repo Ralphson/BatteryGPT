9ab3a7e725ea07ec76459f525289f4da55012bfe
Ralphson <673091663@qq.com>
2024-03-19 21:59:45 +0800

    transfer workspace from local to cloud

--- Time-LLM-main/test.py
+++ Time-LLM-main/test.py
@@ -1,2 +1,17 @@
-with open('./test.txt', 'r') as f:
-    f.write(123)
+from utils.losses import smape_loss, mask_Metrics, mape_loss
+import torch
+
+
+
+
+a = torch.tensor([1]).repeat(10).reshape(2, 5, 1)
+b = torch.tensor([2]).repeat(10).reshape(2, 5, 1)
+m = torch.tensor([1,1,1,1,0,1,1,1,0,0]).reshape(2,5,1)
+
+
+l1 = smape_loss()(0,0,a,b,m)/200
+l2 = mask_Metrics()(a,b,m)
+
+print(l1)
+print(l2)
+
